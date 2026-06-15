2bdc656be11542e34fb1642305916a011e2795cf
anthony <anthonyymartins@gmail.com>
2026-06-15 12:30:04 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    # check for all event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
