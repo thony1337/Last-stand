47a812fe876eb6c684b0414d8e2e5e3b13f5cb72
anthony <anthonyymartins@gmail.com>
2026-06-16 14:09:17 -0300

    Add: star uml code

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
-
 print('Setup Start')
 pygame.init()
 window = pygame.display.set_mode(size=(600, 480))
 
-while True:
-    # check for all event
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
+while True :
+  # check for all event
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          quit()
